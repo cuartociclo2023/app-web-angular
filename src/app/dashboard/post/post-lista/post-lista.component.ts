b5f83b468aae59a9d943f027648241b50aa211f2
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-12-07 22:31:49 -0500

    integración con la vista

--- src/app/dashboard/post/post-lista/post-lista.component.ts
+++ src/app/dashboard/post/post-lista/post-lista.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post-lista',
   templateUrl: './post-lista.component.html',
   styleUrl: './post-lista.component.css'
 })
-export class PostListaComponent {
+export class PostListaComponent implements OnInit {
+  
+  posts: Post[] = []
+
+  constructor(private postService: PostService){
+  }
+  ngOnInit(): void {
+    this.getPosts()
+  }
+  getPosts(){
+    this.postService.getPosts().subscribe(
+      (data) => {
+        this.posts = data        
+      }
+    )
+  }
 
 }
