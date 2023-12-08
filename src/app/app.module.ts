242cdc544e5358eebdfcb4c431731dca7526f763
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-12-07 20:25:27 -0500

    material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './auth/login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialModule } from './angular-material/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    DashboardComponent,    
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  exports: [
+
   ],
   providers: [],
   bootstrap: [AppComponent]
